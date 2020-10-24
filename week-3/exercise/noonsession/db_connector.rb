d4144fdb553f45464a6f6eda8efbc813f57daaf6
Asep Hidayat <rahmaputri13@gmail.com>
2020-10-24 15:40:51 +0700

    edisi pusing commit :)

---
+++ week-3/exercise/noonsession/db_connector.rb
@@ -0,0 +1,23 @@
+require 'mysql2'
+require './item.rb'
+
+def create_db_client
+    client = Mysql2::Client.new(
+        :host => 'localhost',
+        :username => 'root',
+        :password => '1305438asep',
+        :database => 'food_oms_db'
+    )
+    client
+end
+
+def get_all_items
+    client = create_db_client
+    raw_data = client.query("select * from item")
+
+end
+
+def create_new_item(name, price)
+    client = create_db_client
+    client.query("insert into item (name, price) values('#{name}','#{price}')")
+end
--- week-3/exercise/noonsession/main.rb
+++ week-3/exercise/noonsession/main.rb
@@ -1,22 +1,27 @@
 require 'sinatra'
+require './db_connector'
 
 
 get '/list_food' do
     erb :index
 end
 
+# Route to create new food
 get '/new_food' do
     erb :new_food
 end
 
-get '/success' do
-    erb :success
-end
-
 post '/new_food' do
+    create_new_item( params['name'], params['price'])
+    
     unless params['name'].empty? && params['price'].empty?
         return "===== Menu #{params['name']} with price #{params['price']} added! ====="
     else
         redirect '/new_food'
     end
 end
+
+get '/success' do
+    erb :success
+end
+
